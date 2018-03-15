c59e3f69dc4ea903d198ba7d459a4ad8b542dd46
李虎 <lihu@lihudeMacBook-Pro.local>
2018-03-15 08:18:38 +0800

    modify hexMesh

---
+++ Assets/Scripts/Model/HexMesh.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class HexMesh : MonoBehaviour
+{
+	public const float outerRadius = 1f;
+
+	public const float innerRadius = outerRadius * 0.866025404f;
+
+	public static Vector3[] corners = {
+		new Vector3(0f, 0f, outerRadius),
+		new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+		new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+		new Vector3(0f, 0f, -outerRadius),
+		new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+		new Vector3(-innerRadius, 0f, 0.5f * outerRadius),
+		new Vector3(0f, 0f, outerRadius),
+	};
+
+	Mesh hexMesh;
+	List<Vector3> vertices;
+	List<int> triangles;
+	// Use this for initialization
+	void Awake ()
+	{
+		GetComponent<MeshFilter>().mesh = hexMesh = new Mesh();
+		hexMesh.name = "Hex Mesh";
+		vertices = new List<Vector3>();
+		triangles = new List<int>();
+		CreateMesh ();
+	}
+	void CreateMesh()
+	{
+		var center = Vector3.zero;
+		for (int i = 0; i < 6; i++) {
+			AddTriangle(
+				center,
+				center + corners[i],
+				center + corners[i + 1]
+			);
+		}
+		hexMesh.vertices = vertices.ToArray();
+		hexMesh.triangles = triangles.ToArray();
+	}
+	public void SetColor()
+	{
+		
+	}
+	void AddTriangle (Vector3 v1, Vector3 v2, Vector3 v3) {
+		int vertexIndex = vertices.Count;
+		vertices.Add(v1);
+		vertices.Add(v2);
+		vertices.Add(v3);
+		triangles.Add(vertexIndex);
+		triangles.Add(vertexIndex + 1);
+		triangles.Add(vertexIndex + 2);
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
+
